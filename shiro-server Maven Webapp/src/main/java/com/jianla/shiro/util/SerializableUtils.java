9d526a3ed710d41ce1eebe655c1a6c483b66d341
dimhat <dimhat@qq.com>
2015-12-08 20:32:01 +0800

    完成session部署在redis中

--- shiro-server Maven Webapp/src/main/java/com/jianla/shiro/util/SerializableUtils.java
+++ shiro-server Maven Webapp/src/main/java/com/jianla/shiro/util/SerializableUtils.java
@@ -21,48 +21,48 @@ import org.apache.shiro.session.Session;
  */
 public class SerializableUtils {
 
-    public static String serialize(Session session) {
-        try {
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            ObjectOutputStream oos = new ObjectOutputStream(bos);
-            oos.writeObject(session);
-            return Base64.encodeToString(bos.toByteArray());
-        } catch (Exception e) {
-            throw new RuntimeException("serialize error", e);
-        }
-    }
+	public static String serialize(Session session) {
+		try {
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			ObjectOutputStream oos = new ObjectOutputStream(bos);
+			oos.writeObject(session);
+			return Base64.encodeToString(bos.toByteArray());
+		} catch (Exception e) {
+			throw new RuntimeException("serialize error", e);
+		}
+	}
 
-    public static Session deserialize(String sessionStr) {
-        try {
-            ByteArrayInputStream bis = new ByteArrayInputStream(Base64.decode(sessionStr));
-            ObjectInputStream ois = new ObjectInputStream(bis);
-            return (Session) ois.readObject();
-        } catch (Exception e) {
-            throw new RuntimeException("deserialize error", e);
-        }
-    }
+	public static Session deserialize(String sessionStr) {
+		try {
+			ByteArrayInputStream bis = new ByteArrayInputStream(Base64.decode(sessionStr));
+			ObjectInputStream ois = new ObjectInputStream(bis);
+			return (Session) ois.readObject();
+		} catch (Exception e) {
+			throw new RuntimeException("deserialize error", e);
+		}
+	}
 
-    //序列化成字节
-    public static byte[] serializeByte(Session session) {
-        try {
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            ObjectOutputStream oos = new ObjectOutputStream(bos);
-            oos.writeObject(session);
-            return bos.toByteArray();
-        } catch (Exception e) {
-            throw new RuntimeException("serialize error", e);
-        }
-    }
+	//序列化成字节
+	public static byte[] serializeByte(Session session) {
+		try {
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			ObjectOutputStream oos = new ObjectOutputStream(bos);
+			oos.writeObject(session);
+			return bos.toByteArray();
+		} catch (Exception e) {
+			throw new RuntimeException("serialize error", e);
+		}
+	}
 
-    //反序列化字节为session
-    public static Session deserializeByte(byte[] sessionByte) {
-        try {
-            ByteArrayInputStream bis = new ByteArrayInputStream(sessionByte);
-            ObjectInputStream ois = new ObjectInputStream(bis);
-            return (Session) ois.readObject();
-        } catch (Exception e) {
-            throw new RuntimeException("deserialize error", e);
-        }
-    }
+	//反序列化字节为session
+	public static Session deserializeByte(byte[] sessionByte) {
+		try {
+			ByteArrayInputStream bis = new ByteArrayInputStream(sessionByte);
+			ObjectInputStream ois = new ObjectInputStream(bis);
+			return (Session) ois.readObject();
+		} catch (Exception e) {
+			throw new RuntimeException("deserialize error", e);
+		}
+	}
 
 }
